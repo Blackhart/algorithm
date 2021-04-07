40dcc7f061537a559b37f28e3728599c37c576cd
Thomas Manceau <manceau.thomas19@gmail.com>
2021-04-07 15:12:18 +0200

    Add bubble sort algorithm

---
+++ sorting/bubble.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+
+using namespace std;
+
+// Complexity:
+//
+// Space: O(1)
+// Time:
+//    - best case: O(N) -> sorted array
+//    - worst case: O(N*N) -> unsorted array
+//
+vector<int> bubbleSort(vector<int> array) {
+    bool isSorted = false;
+    size_t sortedAtTheHead = 0;
+	
+	while (!isSorted) {
+		isSorted = true;
+		
+		for (size_t i = 0; i < array.size() - 1 - sortedAtTheHead; i++) {
+			if (array[i] > array[i + 1]) {
+				swap(array[i], array[i + 1]);
+				isSorted = false;
+			}
+		}
+		
+		sortedAtTheHead++;
+	}
+	
+  return array;
+}
